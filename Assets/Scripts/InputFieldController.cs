6bb8be9d8bb517247b000b43e175452d701a88ca
linzhaoqian <1203783169@qq.com>
2021-11-16 23:57:39 +0800

    11.16json读取/拖动块

--- Assets/Scripts/InputFieldController.cs
+++ Assets/Scripts/InputFieldController.cs
@@ -1,30 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class InputFieldController : MonoBehaviour
+public class InputFieldController : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
     // Start is called before the first frame update
     public int iLevel;
+    public string contents;
+    
+    private  InputField inputfield;
+
+    public  Vector2 offsetPos;
     void Start()
     {
-        
+        inputfield = GetComponent<InputField>(); 
+        if (inputfield.text != contents)
+        {
+            inputfield.text = contents;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
+      //临时记录点击点与UI的相对位置
 
-
-    public void Drag()//鼠标拖动事件，变换位置。
+    public void OnDrag(PointerEventData eventData)
     {
-        RectTransform rect = transform.GetComponent<RectTransform>();
-        transform.position = Input.mousePosition-new Vector3 (rect.rect.width/2 ,0,0);
+        transform.position = eventData.position - offsetPos;
     }
 
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        offsetPos = eventData.position - (Vector2)transform.position;
 
+    }
 
 }
--- Assets/Scripts/TreeBulider.cs
+++ Assets/Scripts/TreeBulider.cs
@@ -2,20 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using LitJson;
+using System.IO;
+using UnityEditor;
+
+
+public class Block
+{
+    public string Contents { get; set; }
+    public int Pos { get; set; }//在数组中的位置
+    public int PartentPos { get; set; }//父节点在数组中的位置
+}
+public class BlockList
+{
+    public List<Block> list = new List<Block>();
+}
 
 public class TreeBulider : MonoBehaviour
 {
-    private string Name;
-    private int iChildrenCount;
-    private int iLevel;
-
-    [Serializable]
-    public struct Zhuti
+    public BlockList blockList = null;
+    void Start()
     {
-        public  GameObject  Parent;
-        public GameObject[]  Child ;
-    };
-    public Zhuti[] zhuti;
+        Block block = new Block();
+        block.Contents = "1";
+        block.Pos = 14;
+        block.PartentPos = 231;
+        
+        Save(block);
+        Block block1 = new Block();
+        block1.Contents = "asdadwa";
+        block1.Pos = 12;
+        block1.PartentPos = 1;
+        Save(block1);
+    }
+    public void Save(Block block)
+    {
+        string filePath = Application.dataPath + @"/Data1.json";
+
+        blockList = new BlockList();
+        if (!File.Exists(filePath))
+        {
+            blockList.list.Add(block);
+        }
+        else
+        {
+            bool bFind = false;
+            for (int i = 0; i < blockList.list.Count; i++)
+            {
+                Block saveBlock = blockList.list[i];
+                if (block.Contents == saveBlock.Contents)
+                {
+                    saveBlock.Pos = block.Pos;
+                    saveBlock.PartentPos = block.PartentPos;
+
+                    bFind = true;
+                    break;
+                }
+
+            }
+            if (!bFind)
+            {
+                blockList.list.Add(block);
+            }
+        }
+
+        FileInfo file = new FileInfo(filePath);
+        StreamWriter sw = file.CreateText();
+        string json = JsonMapper.ToJson(blockList.list);
+        sw.WriteLine(json);
+        sw.Close();
+        sw.Dispose();
+        AssetDatabase.Refresh();
+    }
 
 
 }
